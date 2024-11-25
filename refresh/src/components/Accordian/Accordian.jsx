0da444a74703f8832c53b59d77b2a594c9de9ebc
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-11-26 01:13:33 +0300

    final

--- refresh/src/components/Accordian/Accordian.jsx
+++ refresh/src/components/Accordian/Accordian.jsx
@@ -5,20 +5,35 @@ import data from './data'
 const  Accordian = ( ) =>{
 
     const [selected, setSelected]= useState(null)
+
+     function handleSingleSelection(getCurrentId){
+       setSelected(getCurrentId)
+
+
+     }
+
+
     return(
     <div className="container">
         <div className="accordian"> 
           {
             data && data.length ? data.map((dataItem)=>(
                 <div className="item">
-                    <div className="title">
+                    <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
                         <h3>{dataItem.question}</h3>
                         <span>+</span>
+
+                   
+
                 </div>
+                {selected===dataItem.id?<div className='answer'>{dataItem.answer} </div>:null}
                     
                      </div>
+
+                   
             )) : <div> There is no Data Avialable </div>
           }
+         
         </div>
  
     </div> )
