7d9a5acbbcd7a676edb8fb354407b4af8a5902c2
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-11-25 23:10:55 +0300

    greatStart

---
+++ refresh/src/App.js
@@ -0,0 +1,8 @@
+const data = [
+    { id: 1, question: "What is your name?", answer: "My name is Tina Dessu." },
+    { id: 2, question: "Where do you live?", answer: "I live in Addis Ababa." },
+    { id: 3, question: "What is your profession?", answer: "I am a front-end developer." },
+    { id: 4, question: "What technologies do you work with?", answer: "I primarily work with React and the MERN stack." },
+    { id: 5, question: "What is your educational background?", answer: "I have a degree in Software Engineering." }
+  ];
+  
